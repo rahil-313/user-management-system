bb42b17333a9945daeae93584e5dbc250fdbf228
rahil-313 <sayyadrahil275@gmail.com>
2026-04-17 14:50:45 +0530

    backend ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
